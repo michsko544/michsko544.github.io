cfbc4cc67fd27a879bd7b07a231c069fc31abc59
Majkel <michsko544@student.polsl.pl>
2021-02-23 17:03:20 +0100

    Add glitch effect

--- src/script.js
+++ src/script.js
@@ -78,3 +78,36 @@ document.querySelectorAll(".toggle-menu").forEach(function(node){
         menuVisibility=true;
     }
 }})
+
+//Glitch effect
+
+    var bg = document.querySelector(".profile-img");
+    var count = 10;
+    for(var i = 0; i<count; i++){
+        var glitchBox = document.createElement("div");
+        glitchBox.className = "glitch-box";
+        bg.appendChild(glitchBox);
+    }
+
+setInterval(function(){
+    var maxRepeats = Math.floor(Math.random()*3+3);
+    var term = Math.floor(Math.random()*200 + 50);
+    var interval = setInterval(function(){
+        var glitch = document.getElementsByClassName("glitch-box");
+        for(var i = 0; i< glitch.length; i++){
+            glitch[i].style.left = Math.floor(Math.random()*100) + "%";
+            glitch[i].style.top = Math.floor(Math.random()*100) + "%";
+            glitch[i].style.width = Math.floor(Math.random()*300) + "px";
+            glitch[i].style.height = Math.floor(Math.random()*150) + "px";
+        }
+    },term);
+
+    setTimeout(function(){
+        clearInterval(interval);
+        var glitch = document.getElementsByClassName("glitch-box");
+        for(var i = 0; i< glitch.length; i++){
+            glitch[i].style.width = "0px";
+            glitch[i].style.height = "0px";
+        }
+    }, maxRepeats*term)
+}, 4000 + Math.floor(Math.random()*4000))
